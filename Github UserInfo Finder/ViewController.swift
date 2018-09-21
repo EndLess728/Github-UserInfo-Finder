7e4c162310d4f9df48c7a4ec9312ae0df6869c63
EndLess728 <aryanmk.mk5@gmail.com>
2018-09-21 10:49:40 +0530

    This app will find Github users Name , repos, followers, location etc by searching their username

--- Github UserInfo Finder/ViewController.swift
+++ Github UserInfo Finder/ViewController.swift
@@ -8,18 +8,107 @@
 
 import UIKit
 
+struct MyGithub: Codable {
+    let name:String?
+    let followers:Int?
+    let location:String?
+    let public_repos:Int?
+    let avatar_url:URL?
+    
+
+    
+}
+
 class ViewController: UIViewController {
 
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var userImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var followerLabel: UILabel!
+    @IBOutlet weak var repoLabel: UILabel!
+    
+    @IBOutlet weak var nameTitle: UILabel!
+    @IBOutlet weak var locationTitle: UILabel!
+    @IBOutlet weak var followersTitle: UILabel!
+    @IBOutlet weak var repoTitle: UILabel!
+    
+
+    
+    @IBAction func findUserButton(_ sender: Any) {
+        
+        let userText = usernameTextField.text
+        
+        guard let gitUrl = URL(string: "https://api.github.com/users/" + userText!) else {return}
+        
+        URLSession.shared.dataTask(with: gitUrl) { (data, response, error) in
+            
+            guard let data = data else {return}
+            do {
+                
+                let decoder = JSONDecoder()
+                let gitData = try decoder.decode(MyGithub.self, from: data)
+                
+                DispatchQueue.main.sync {
+            
+                    if let gimage = gitData.avatar_url{
+                        let data = try? Data(contentsOf: gimage)
+                        let image: UIImage = UIImage(data: data!)!
+                        self.userImageView.image = image
+                    }
+                    
+                    if let gname = gitData.name{
+                        self.nameLabel.text = gname
+                    }
+                    
+                    if let glocation = gitData.location {
+                        self.locationLabel.text = glocation
+                    }
+                    
+                    if let gfollowers = gitData.followers {
+                        self.followerLabel.text = String(gfollowers)
+                    }
+                    
+                    if let grepos = gitData.public_repos{
+                        self.repoLabel.text = String(grepos)
+                    }
+                }
+                
+                
+                    
+                    
+                }
+                
+            catch let err {
+                print("Err", err)
+            
+        }
+        
+    }.resume()
+        setLabelStatus(value: false)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        setLabelStatus(value: true)
+        
+        
+}
+    func setLabelStatus(value: Bool) {
+        nameLabel.isHidden = value
+        locationLabel.isHidden = value
+        followerLabel.isHidden = value
+        repoLabel.isHidden = value
+        userImageView.isHidden = value
+        nameTitle.isHidden = value
+        locationTitle.isHidden = value
+        repoTitle.isHidden = value
+        followersTitle.isHidden = value
+        
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+
 
 
 }
-
